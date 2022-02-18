7c4298ae19edd954afbb9323a97db5c982800411
Ravi Shekhar <rsshekhar27@outlook.com>
2022-02-18 08:47:57 -0600

    Added rest teplate example in test

--- src/test/java/com/rs/springframework/SpringRestClientExamplesApplicationTests.java
+++ src/test/java/com/rs/springframework/SpringRestClientExamplesApplicationTests.java
@@ -9,8 +9,8 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest
 class SpringRestClientExamplesApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
+//    @Test
+//    void contextLoads() {
+//    }
 
 }
